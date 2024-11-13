48df8a6d5c3a0f82d0209e893b348f38ea1c5023
tkgka <ksh17171@gmail.com>
2024-11-13 20:12:11 +0900

    Initial Commit

---
+++ OnboardingExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnboardingExample
+//
+//  Created by 김수환 on 11/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnboardingExample/OnboardingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingExampleApp.swift
+//  OnboardingExample
+//
+//  Created by 김수환 on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
